633d6a663e691fc3dfd8e9e8571ecfcb23b1af79
geonmu <2leegm@naver.com>
2023-11-09 04:44:07 +0900

    kwworld

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,91 +1,94 @@
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+
+function surfingClick() {
+    alert('pado');
+}
 
 function Profile() {
     return(
-        <ProfileBox>
-            <Today>gd</Today>
-            <InnerProfileBox>
-                <ProfileImage src = "http://res.heraldm.com/content/image/2021/07/16/20210716000671_0.jpg" />
-                <Intro></Intro>
-            </InnerProfileBox>
-        </ProfileBox>
+        <>
+            <div>
+                <ProfileImage src='http://res.heraldm.com/content/image/2021/07/16/20210716000671_0.jpg'/>
+            </div>
+            <div>
+                <DottedLine />
+                <TodayIs>
+                    <p><span style={{ color: '#387291' }}>TODAY IS...</span></p>&nbsp;졸림
+                </TodayIs>
+            </div>
+            <div>
+                <BodyText>인트로 최대 4줄<br/>ㅎㅇ<br/>테스트g<br/>test</BodyText>
+            </div>
+            <div>
+                <History>
+                    <p>
+                        <span style={{ color: '#468baf' }}>▶</span>History</p>
+                </History>
+                <BodyText>
+                    <span style={{ color: '#595959' }}>이건무</span>
+                    <span style={{ color: '#aaaaaa', fontSize: 14 }}> 2004.03.09</span><br/>
+                    <span style={{ color: "#ee8f4f" }}> 2leegm@naver.com</span>
+                </BodyText>
+            </div>
+            <div>
+                <Surfing onClick={surfingClick}>파도타기</Surfing>
+            </div>
+        </>
     );
 }
 
 export default Profile;
 
-const ProfileBox = styled.div`
+const TodayIs = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    margin: 10px 0px 0px 0px;
+
+    height: 75%;
+    font-size: 1rem;
     background-color: #ffffff;
-
-    border-radius: 12px;
-    border: 1px solid #374851;
-
-    width: 32%;
-    height: 98%;
-    margin: 6px 0px 0px 2px;
+    border-radius: 8px;
+    border: 2px solid #d0d0ce;
 `;
-
-const Today = styled.div`
-    margin: 25px 0px 10px 0px;
-    text-align: center;
-    font-size: 0.8rem;
-`;
-
-const InnerProfileBox = styled.div`
-    background-color: #ffffff;
-
-    border-radius: 12px;
-    border: 8px solid #ececec;
-    box-shadow:
-        0 0 0 2px hsl(0, 0%, 70%);
-
-    width: 92%;
-    height: 88%;
-    margin: 0px auto;
-`;
-
 
 const ProfileImage = styled.img`
-    width: 210px;
-    height: 140px;
-
+    width: 100%;
+    height: 100%;
     border-radius: 12px;
-
-    margin: 20px auto 0px auto;
-    display: block;
-
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center;
+    object-fit: cover;
 `;
 
-const Intro = styled.div`
-    width: 80%;
-    height: 30%;
-
-    margin: 25px auto 10px auto;
-
-    border-width: 2px 0px 2px 0px;
-    border-color: #d7d7d7;
-    border-style: dotted;
+const BodyText = styled.div`
+    margin: 10px 0px 0px 0px;
+    line-height: 1.15;
 `;
 
 const History = styled.div`
-
+    height: 20%;
+    border-bottom: 2px solid #dddddd;
+    font-size: 0.8rem;
+    font-weight: 600;
+    color: #1ea7cc;
 `;
 
-const UserName = styled.div`
-
+const Surfing = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    font-size: 1rem;
+    background-color: #ffffff;
+    border-radius: 4px;
+    border: 1px solid #d0d0ce;
+    box-shadow: 2px 2px rgb(0, 0, 0, 0.3);
+    cursor: pointer;
 `;
 
-const UserBirthday = styled.div`
-
-`;
-
-const UserEmail = styled.div`
-
-`;
-
-const Pado = styled.div`
-
+const DottedLine = styled.div`
+    border: none;
+    border-top: 2px dotted #afafaf;
+    height: 1px;
 `;
--- src/pages/MinihompyPage.jsx
+++ src/pages/MinihompyPage.jsx
@@ -1,13 +1,189 @@
-import Layout from '../components/Layout'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Profile from '../components/Profile'
 
 function MinihompyPage() {
+    const [content, setContent] = useState();
+
+    const handleClickButton = e => {
+        const { name } = e.target;
+        setContent(name);
+    };
+
+    /*
+    const selectComponent = {
+        main: <Main />,
+        diary: <Diary />,
+        guestbook: <Guestbook />,
+    };
+    */
+
+
     return (
-        <>
-        <Layout>
-            
-        </Layout>
-        </>
+    <Background>
+        <BookCover>
+            <BookDot>
+                <ProfileBox>
+                    <HeadText style={{ justifyContent: 'center' }}>
+                        <p>TODAY<span style={{ color: '#a96a6a' }}> 12</span> | TOTAL 34</p>
+                    </HeadText>
+                    <InnerProfileBox>
+                        <Profile/>
+                    </InnerProfileBox>
+                </ProfileBox>
+                <ContentBox>
+                    <ContentHead>
+                        <HeadText style={{ justifyContent: 'left' }}>
+                            <p>TODAY<span style={{ color: '#a96a6a' }}> 12</span> | TOTAL 34</p>
+                        </HeadText>
+                        <HeadText style={{ justifyContent: 'right' }}>
+                            <p>TODAY<span style={{ color: '#a96a6a' }}> 12</span> | TOTAL 34</p>
+                        </HeadText>
+                    </ContentHead>
+
+                    
+                    <InnerContentBox>
+                        asdf
+                    </InnerContentBox>
+                </ContentBox>
+            </BookDot>
+        </BookCover>
+    </Background>
     );
 }
 
 export default MinihompyPage;
+
+const Background = styled.div`
+    background-size: 30px 30px;
+    background-image:
+      linear-gradient(to right, #919191 1px, transparent 1px),
+      linear-gradient(to bottom, #919191 1px, transparent 1px);
+    background-color: #767676;
+
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+`;
+
+const BookCover = styled.div`
+    background-color: #6faed4;
+
+    border-radius: 12px;
+    border: 2px solid #374851;
+    box-shadow: 5px 5px 5px rgb(0, 0, 0, 0.3);
+
+    width: 1000px;
+    height: 600px;
+    margin: 100px auto;
+
+    position: relative;
+`;
+
+const BookDot = styled.div`
+    border: 3px dashed #b9d8e5;
+    border-radius: 12px;
+
+    top: 25px;
+    bottom: 25px;
+    left: 25px;
+    right: 25px;
+    
+    position: absolute;
+
+    padding: 5px 3px 5px 3px;
+
+    display: grid;
+    grid-template-columns: 32% 68%;
+    grid-template-rows: 100%;
+`;
+
+const ProfileBox = styled.div`
+    background-color: #ffffff;
+
+    border-radius: 12px;
+    border: 1px solid #374851;
+
+    padding: 12px 9px 12px 9px;
+
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr 19fr;
+    row-gap: 6px;
+`;
+
+const ContentBox = styled.div`
+    background-color: #eeeeee;
+
+    border-radius: 12px;
+    border: 1px solid #374851;
+
+    padding: 12px 9px 12px 9px;
+
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr 19fr;
+    row-gap: 6px;
+`;
+
+const ContentHead = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr;
+
+    padding: 0px 8px 0px 8px;
+`;
+
+
+const HeadText = styled.div`
+    display: flex;
+    align-items: flex-end;
+
+    font-size: 1rem;
+`;
+
+const InnerProfileBox = styled.div`
+    background-color: #ffffff;
+
+    border-radius: 12px;
+    border: 2px solid #b3b3b3;
+    box-shadow: inset 0 0 0 6px hsl(0, 0%, 90%);
+
+    padding: 25px 30px 25px 30px;
+
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 3.5fr 1fr 2.25fr 2fr 0.75fr;
+    row-gap: 3%;
+`;
+
+const InnerContentBox = styled.div`
+    background-color: #ffffff;
+
+    border-radius: 12px;
+    border: 2px solid #b3b3b3;
+    box-shadow: 2px 2px 2px rgb(0, 0, 0, 0.2);
+
+    padding: 25px 90px 25px 90px;
+
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 3.5fr 1fr 2.25fr 2fr 0.75fr;
+    row-gap: 3%;
+`;
+
+const Menu = styled.div`
+background-color: #ffffff;
+
+border-radius: 12px;
+border: 1px solid #374851;
+
+padding: 15px 12px 15px 12px;
+
+display: grid;
+grid-template-columns: 1fr;
+grid-template-rows: 1fr 19fr;
+row-gap: 6px;
+`;
